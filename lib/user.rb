bc9a01a65f54b12bab98e1b4423f583938246ff1
isabella g henriques <isabellagh88@gmail.com>
2020-06-12 03:58:02 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,6 +1,16 @@
+
+
 class User 
   
   attr_accessor :first_name, :last_name, :knowledge
+  
+  def initialize
+    @knowledge = []
+  end 
+  
+  def learn(string)
+    @knowledge << string
+  end 
   
   
 end 
